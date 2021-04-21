5b51b1bd46ad7b3bd24c8177178e26ce2ec911f0
Piyush <piyushgargofficial@gmail.com>
2021-04-21 18:39:54 +0530

    Final Commit

--- mernstack/src/Components/Register.js
+++ mernstack/src/Components/Register.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import {login} from '../Images/login.jpg'
 
 export default function Home() {
     return (
-        <div>
-            <p>Welcome Register</p>
-            <h1>We are the MERN Developer</h1>
-        </div>
+        <>
+            
+        </>
     )
 }
